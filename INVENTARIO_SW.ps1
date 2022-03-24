7009b42324b542059d792fc4d432428aaa60ec8c
GOTXE <99086747+GOTXE@users.noreply.github.com>
2022-03-24 20:29:56 +0100

    Finalizado

--- INVENTARIO_SW.ps1
+++ INVENTARIO_SW.ps1
@@ -2,17 +2,18 @@
 #                           INVENTARIO DE SOFTWARE PC                                   #
 #########################################################################################
 #                                                                                       #
-# ESTE SCRIPT CREA UN INVENTARIO DE SOFTWARE DE LOS PC A LOS QUE SE APLIQUE             #
-# EXPORTANDO LOS DATOS POR CADA EQUIPO EN FORMATO TXT                                   # 
+#      ESTE SCRIPT CREA UN INVENTARIO DE SOFTWARE DE LOS PC A LOS QUE SE APLIQUE        #
+#             EXPORTANDO LOS DATOS POR CADA EQUIPO EN FORMATO TXT                       # 
 #                                                                                       #
 #########################################################################################
 # EN LA VARIABLE $path TIENES QUE ESPECIFICAR LA CARPETA DONDE GUARDARÁ EL txt          #
-#########################################################################################
-# GUARDAR EL ARCHIVO INVENTARIO_SW.PS1 EN UN DIRECTORIO DONDE TENGAN ACCESO TODOS LOS   #
-# PC DE LOS QUE SE QUIERA HACER INVENTARIO.                                             # 
+# EN LA VARIABLE $fileLog TIENES QUE ESPECIFICAR LA RUTA DONDE SE GUARDARÁ EL LOG       #
 #########################################################################################
 # ESTE SCRIPT GENERA UN NOMBRE DE ARCHIVO QUE EMPIEZA POR TU                            #
 # HAY QUE MODIFICAR A MANO LOS DC (DCXX) Y LOS TERMINALES DE ADMINISTRADOR (TUADMXX)    #
+#########################################################################################
+# DIEGO                                                                                 #   
+# Marzo, 2022                                                                           #  
 #########################################################################################
 
 
