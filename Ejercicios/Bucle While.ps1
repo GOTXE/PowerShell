c711d8a6546599a3d3bed7c1d3042fd1942d8309
GOTXE <99086747+GOTXE@users.noreply.github.com>
2022-05-27 13:20:57 +0200

    Add files via upload

---
+++ Ejercicios/Bucle While.ps1
@@ -0,0 +1,14 @@
+﻿# Escribe el nombre tantas veces commo el numero
+# que se ha solicitado
+
+
+$nombre = Read-Host "Introduce tu nombre"
+$numero = Read-Host "Introduce un número"
+$reps = 0
+
+    while ($reps -ne $numero) {
+        
+        Write-Host $nombre
+        $reps++
+    
+    }
---
+++ Ejercicios/Ejercicio strings.ps1
@@ -0,0 +1,3 @@
+﻿$nombre = Read-Host "Introduce un nombre"
+
+Write-Host $nombre.ToUpper() "tiene" $nombre.Length "letras"
