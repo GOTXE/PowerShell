6333788493fdd6420a07d203318c36111e901a5b
GOTXE <soltero_cautivado.0i@icloud.com>
2022-08-09 13:04:00 +0200

    CuotaDiscos

---
+++ SRV/CuotaDiscos.ps1
@@ -0,0 +1,4 @@
+# Si no aparece Administracion de recursos del servidor de archivos #
+# en Herramientas > Administracion del servidor, ejecutar ....      #
+
+Install-WindowsFeature -Name FS-Resource-Manager -IncludeManagementTools
