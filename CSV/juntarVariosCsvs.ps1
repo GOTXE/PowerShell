d323e47f6b22099aca4a4ae516107dd2fd5efe2d
GOTXE <99086747+GOTXE@users.noreply.github.com>
2022-08-16 21:14:14 +0200

    .

--- CSV/juntarVariosCsvs.ps1
+++ CSV/juntarVariosCsvs.ps1
@@ -1,9 +1,10 @@
 # Juntar varios csv en uno 
 # Primero nombrar por orden los csv que se quieren unir
 
-$folderCsv = PATH\
-$nombreCsvNuevo = Nombre.csv
+$folderCsv = 'PATH\'
+$archivosCsv = $folderCsv+'*.csv'
+$nombreCsvNuevo = 'Nombre.csv'
 $pathCsvNuevo = $folderCsv + $nombreCsvNuevo
 
 
-Get-Content -Filter *.csv -Path $folderCsv*.csv | Add-Content -path nombre.csv
+Get-Content -Filter *.csv -Path $archivosCsv | Add-Content -path $pathCsvNuevo
