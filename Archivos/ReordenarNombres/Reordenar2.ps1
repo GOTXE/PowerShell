9151d7823d59c9d0b7a1da1b48cffa0a4d8ca6ba
GOTXE <99086747+GOTXE@users.noreply.github.com>
2022-11-27 19:40:13 +0100

    Reordenar corregido

---
+++ Archivos/ReordenarNombres/Reordenar2.ps1
@@ -0,0 +1,82 @@
+$path = "PATH"
+
+
+Get-ChildItem -path $path| where {$_.Name -match 'UF+ '} | ForEach-Object{
+
+    Rename-Item -Path $_.FullName -NewName ($_.Name -replace ('UF+ ','UF')) -ErrorAction SilentlyContinue # Cambia UF\sXXXX  por UFXXXX
+
+} 
+
+Get-ChildItem -path $path | where {$_.Name -match "Prueba[a-z,' ',-]Pr[\w]ctica" -or "Pr[\w]ctica"} | ForEach-Object {
+
+    Rename-Item -Path $_.FullName -NewName ($_.Name -replace ("Prueba[a-z,' ',-]Pr[\w]ctica","")) -ErrorAction SilentlyContinue # Cambia Prueba Practica por nada
+    Rename-Item -Path $_.FullName -NewName ($_.Name -replace ("Pr[\w]ctica","")) -ErrorAction SilentlyContinue # Cambia Practica por nada
+
+   }
+
+Get-ChildItem -Path $path | where {$_.Name -match "\.{2,}"} | ForEach-Object {
+
+    Rename-Item -Path $_.FullName -NewName ($_.Name -replace ("\.{2,}",".")) -ErrorAction SilentlyContinue # Cambia .. por .
+
+} 
+
+Get-ChildItem -Path $path | where {$_.Name -match "MF0080-2-??"} | ForEach-Object {
+
+    Rename-Item -Path $_.FullName -NewName ($_.Name -replace("MF0080-2-?",""))  # Elimina MF0080-2- 
+         
+}
+
+Get-ChildItem -Path $path | ForEach-Object {
+
+   Rename-Item -Path $_.FullName -NewName ($_.Name.Trim()) # Elimina los espacios iniciales
+}
+
+
+Get-ChildItem -path $path| where {$_.Name -match "\w\d{4}\w??\s{2}"} | ForEach-Object{
+
+    Rename-Item -Path $_.FullName -NewName ($_.Name -replace ("  "," - ")) -ErrorAction SilentlyContinue # Corrige 2 espacios a \s-\s despues de UFXXX - .....
+
+} 
+
+Get-ChildItem -path $path| where {$_.Name -notmatch "\w\d{4}\w??\s-"} | ForEach-Object{
+    
+     $cuenta = $_.Name.IndexOf(' ')
+
+     Rename-Item -Path $_.FullName -NewName ($_.Name.Remove($cuenta,1).Insert($cuenta," - ")) -ErrorAction SilentlyContinue # Cambia el nombre UFXXX L... y UFXXXXD M... a \s-\s
+
+}
+
+Get-ChildItem -path $path| where {$_.Name -notmatch 'E\d -'} | ForEach-Object{
+   
+     
+    Rename-Item -Path $_.FullName -NewName ( $_.Name -replace '(E\d{1}) -(\d{1})', '$1-$2') -ErrorAction SilentlyContinue # Elimina el \s entre E\d -\d
+    Rename-Item -Path $_.FullName -NewName ( $_.Name -replace '(E\d{1})- (\d{1})', '$1-$2') -ErrorAction SilentlyContinue # Elimina el \s entre E\d- \d
+
+}
+
+
+
+<#
+
+Get-ChildItem -path $path| where {$_.Name -notmatch '^UF\d{4}\w??\s-\sE\d'} | ForEach-Object{
+   
+     
+    Rename-Item -Path $_.FullName -NewName ( $_.Name -replace '(E\d{1}) -(\d{1})', '$1-$2') -ErrorAction SilentlyContinue # Elimina el \s entre E\d -\d
+}
+
+#>
+
+
+
+Get-ChildItem -Path $path |Where-Object {$_.Name -notmatch '^UF\d{4}\w??\s-\sE\d'} | ForEach-Object {
+
+Rename-Item -Path $_.FullName -NewName ( $_.Name -replace '^(UF\d{4}\w??) - (.*?) (E\d{1}-\d{1})', '$1 - $3 - $2' ) -ErrorAction SilentlyContinue # trae adelante EX-X
+
+}
+
+
+Get-ChildItem -Path $path |Where-Object {$_.Name -notmatch '^UF\d{4}\w??\s-\sE\d'} | ForEach-Object {
+
+ Rename-Item -Path $_.FullName -NewName ( $_.Name -replace '^(UF\d{4}\w??) - (.*?) (E\d{1}\s??-??\d??)', '$1 - $3 - $2' ) # Mueve el valor E\d a su lugar
+
+}
