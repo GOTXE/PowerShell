fb435028e6c2ac20272c7df0d65daf488d6bf2c9
GOTXE <99086747+GOTXE@users.noreply.github.com>
2022-11-27 14:27:49 +0100

    Reordenar Nombres Archivo

---
+++ ReordenarNombres/ReordenarNombreArchivo.ps1
@@ -0,0 +1,48 @@
+$path = "C:\Users\GTX\Desktop\PS_BatchRename\Olaya\Evaluación\Diego Caro Soto"
+
+
+Get-ChildItem -path $path| where {$_.Name -match 'UF+ '} | ForEach-Object{
+
+    Rename-Item -Path $_.FullName -NewName ($_.Name -replace ('UF+ ','UF')) -ErrorAction SilentlyContinue # Cambia UF\sXXXX  por UFXXXX
+
+} 
+
+Get-ChildItem -path $path | where {$_.Name -match "Prueba[a-z,' ',-]Pr[\w]ctica" -or "Pr[\w]ctica"} | ForEach-Object {
+
+    Rename-Item -Path $_.FullName -NewName ($_.Name -replace ("Prueba[a-z,' ',-]Pr[\w]ctica","")) -ErrorAction SilentlyContinue # Cambia Prueba Practica por nada
+    Rename-Item -Path $_.FullName -NewName ($_.Name -replace ("Pr[\w]ctica","")) -ErrorAction SilentlyContinue # Cambia Practica por nada
+
+   }
+
+Get-ChildItem -Path $path | where {$_.Name -match "\.{2,}"} | ForEach-Object {
+
+    Rename-Item -Path $_.FullName -NewName ($_.Name -replace ("\.{2,}",".")) -ErrorAction SilentlyContinue # Cambia .. por .
+
+} 
+
+Get-ChildItem -Path $path | where {$_.Name -match "MF0080-2-??"} | ForEach-Object {
+
+    Rename-Item -Path $_.FullName -NewName ($_.Name -replace("MF0080-2-?",""))  # Elimina MF0080-2- 
+         
+}
+
+Get-ChildItem -Path $path | ForEach-Object {
+
+   Rename-Item -Path $_.FullName -NewName ($_.Name.Trim()) # Elimina los espacios iniciales
+}
+
+
+Get-ChildItem -path $path| where {$_.Name -match "\w\d{4}\w??\s{2}"} | ForEach-Object{
+
+    Rename-Item -Path $_.FullName -NewName ($_.Name -replace ("  "," - ")) -ErrorAction SilentlyContinue # Corrige 2 espacios a \s-\s despues de UFXXX - .....
+
+} 
+
+Get-ChildItem -path $path| where {$_.Name -notmatch "\w\d{4}\w??\s-"} | ForEach-Object{
+    
+     $cuenta = $_.Name.IndexOf(' ')
+
+     Rename-Item -Path $_.FullName -NewName ($_.Name.Remove($cuenta,1).Insert($cuenta," - ")) -ErrorAction SilentlyContinue # Cambia el nombre UFXXX L... y UFXXXXD M... a \s-\s
+
+}
+
